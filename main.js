da94a1073641af26db74e14f39221d3d97c939e3
rishavch0udhary <rishavchoudharysupercell@gmail.com>
2025-06-23 10:30:40 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+//  Logical && and Logical || 
+
+let a = true;
+let b = false;
+
+console.log(a && b); // true
+console.log(a || b); // true
+
+
+// Template Literals 
+
+let name1= "John";
+let name2 = "Doe";
+
+console.log(`Hello, ${name1} ${name2}!`); // Hello, John Doe!
+
+
+// Ternary Operator
+
+
+
